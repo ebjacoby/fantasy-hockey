04beb637dd3b69df01a4edd3e029722e4c44fe7d
ebjacoby <65738833+ebjacoby@users.noreply.github.com>
2020-06-29 15:23:56 -0400

    lists of player names and positions

--- app/fantasy_hockey.py
+++ app/fantasy_hockey.py
@@ -22,24 +22,35 @@ for x in current_teams:
 
 print(team_id_list)
 
-
-
-#roster info
+#roster info + player IDs
 
 player_id_list = []
+player_name_list = []
+player_position_list = []
 
-for y in team_id_list:
-    request_url = f"https://statsapi.web.nhl.com/api/v1/teams/{y}/roster"
+for x in team_id_list:
+    request_url = f"https://statsapi.web.nhl.com/api/v1/teams/{x}/roster"
     response_rosters = requests.get(request_url)
         
     parsed_response_rosters = json.loads(response_rosters.text)
 
     current_rosters = parsed_response_rosters["roster"]
 
+    for y in current_rosters:
+        player_id_list.append(int(y["person"]["id"]))
+    
     for z in current_rosters:
-        player_id_list.append(int(z["person"]["id"]))
+        player_name_list.append(z["person"]["fullName"])
+    
+    for a in current_rosters:
+        player_position_list.append(a["position"]["abbreviation"])
+
 
 print(player_id_list)
+print(player_name_list)
+print(player_position_list)
+
+
 # Instructions
 
 #TODO: list of team IDs for the code to loop through when gathering info on specific player IDs
